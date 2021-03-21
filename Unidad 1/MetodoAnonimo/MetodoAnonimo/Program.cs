43571c871085d1a834f7ce3a4d6cb4b6a86df7a3
CaroEcc <73487960+CaroEcc@users.noreply.github.com>
2021-03-21 17:17:17 -0400

    Ejercicio Métodos Anónimos. Programación Avanzada

---
+++ Unidad 1/MetodoAnonimo/MetodoAnonimo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MetodoAnonimo
+{
+    delegate int Aleatorio(int max);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aleatorio alea = delegate (int max)
+            {
+                int promedio=0;
+                int[] array = new int[5];
+                Random random = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(0, max);
+                }
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    promedio = promedio + array[i];
+                }
+                promedio = promedio * array.Length;
+                return promedio;
+            };
+
+            
+            Console.WriteLine("El promedio es:" + alea(6));
+
+        }
+    }
+}
