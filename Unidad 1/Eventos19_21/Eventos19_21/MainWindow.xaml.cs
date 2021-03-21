43de54a698ceb8b0158111ea7b42d624618c3964
CaroEcc <73487960+CaroEcc@users.noreply.github.com>
2021-03-21 17:14:57 -0400

    Primer ejercicio sobre eventos. Programación Avanzada.

---
+++ Unidad 1/Eventos19_21/Eventos19_21/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eventos19_21
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+          
+            window2.Cambiar += new Window2.CambiarTitulo(ejecutar);
+            window2.Show();
+        }
+
+        public void ejecutar(string dato)
+        {
+            win1.Title = dato;
+        }
+            
+      
+    }
+}
---
+++ Unidad 1/Eventos19_21/Eventos19_21/Window2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Eventos19_21
+{
+    /// <summary>
+    /// Lógica de interacción para Window2.xaml
+    /// </summary>
+    
+    public partial class Window2 : Window
+    {
+        public delegate void CambiarTitulo(string dato);
+        public event CambiarTitulo Cambiar;
+
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+
+        {
+            string titulo = txtTitulo.Text;
+
+            Cambiar(txtTitulo.Text); 
+
+        }
+
+
+    }
+}
